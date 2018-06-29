b9ef678966381dfe2f99d801b30416ffd1836562
enolynowr <devkimwr@gmail.com>
2018-06-29 09:16:31 +0900

    Update Constants.java

--- app/src/main/java/com/eatour/hyunjongkim/weatherfood/Constants.java
+++ app/src/main/java/com/eatour/hyunjongkim/weatherfood/Constants.java
@@ -2,9 +2,9 @@ package com.eatour.hyunjongkim.weatherfood;
 
 public interface Constants {
 
-    String GOOGLE_IMAGE_API_KEY = "AIzaSyB7Dy56KtNdN4RURdAeJwAc-BpeaBxGaJU";
-    String GOOGLE_IMAGE_API_SEARCH_ENGINE_ID = "005080994123613126168:hwvodo2w-9a";
-    String WEATHER_API_KEY = "b163330afcea03fe1c511ff3e76b5562";
+    String GOOGLE_IMAGE_API_KEY = "";
+    String GOOGLE_IMAGE_API_SEARCH_ENGINE_ID = "";
+    String WEATHER_API_KEY = "";
     String IMAGE_SEARCH_TYPE = "image";
     String WEATHER_CONDITION_ID_PRE_WORD = "wci_food_";
     String WEATHER_ICON_ID_PRE_WORD = "wci_icon_";
